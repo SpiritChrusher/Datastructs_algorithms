e5648e371db1c4cb569ed03206e0fdf918683ff1
SpiritChrusher <szabokristof997@gmail.com>
2021-02-08 23:54:52 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Numerics;
+
+int[] vector = new[] { 4, 8, 10, 13, 16, 17, 21, 25, 26, 27, 31, 35, 37, 41, 44, 46, 47, 50, 51, 52, 54, 58, 62, 64, 65, 68, 71, 73 };
+Console.WriteLine(vector.Length);
+
+Console.WriteLine($" megoldás: {full_search_for(vector, 17)}");
+
+Console.WriteLine($" rekurzív megoldás: {full_search_rec(vector, 17)}");
+
+
---
+++ Searches.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructs_algorithms
+{
+    public static class Searches
+    {
+       public static int full_search_while(int[] vec, int value)
+        {
+            int i = 0;
+            while (i < vec.Length && vec[i] != value)
+            {
+                i++;
+            }
+            if (i > vec.Length)
+            {
+                Console.WriteLine("there is no such element");
+                return -1;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        public static int full_search_for(int[] vec, int value)
+        {
+            for (int i = 0; i < vec.Length; i++)
+            {
+                if (vec[i] == value)
+                {
+                    return i;
+                }
+            }
+            Console.WriteLine("Wasn't found");
+            return -1;
+        }
+
+        public static int full_search_rec(int[] vec, int value)
+        {
+
+            if (vec.Length == 0)
+            {
+                Console.WriteLine("empty array");
+                return -1;
+            }
+            else if (vec[0] == value)
+            {
+                return 0;
+            }
+            else
+            {
+                return full_search_rec(vec[1..^0], value) + 1;
+            }
+        }
+
+        public static int full_search_rec_mine(int[] vec, int value, int i) // itt kell egy 0 kezdőérték paraméternek
+        {
+            if (vec.Length == 0)
+            {
+                Console.WriteLine("empty array");
+                return -1;
+            }
+            else if (vec[i] == value)
+            {
+                return i;
+            }
+            else
+            {
+                i++;
+                return full_search_rec_mine(vec, value, i);
+            }
+        }
+    }
+}
